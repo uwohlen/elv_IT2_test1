fde163020d2fb4b6e091538f3a814aa4dce4de71
Ruben Jørgensen <56695255+Exelebrake@users.noreply.github.com>
2022-09-16 14:49:11 +0200

    Create testkode1(2).py

---
+++ Ruben/testkode1(2).py
@@ -0,0 +1,18 @@
+import random
+
+batteri = random.randint(1,100)
+
+halv = 50
+
+lavt = 10
+
+print("Batteri nivået er", batteri)
+
+if batteri  < halv and batteri > lavt:
+    print("Batteriet er middels fullt")
+    
+elif batteri < lavt:
+    print("Batteriet er lavt")
+
+else:
+    print("Batteriet er høyt")
