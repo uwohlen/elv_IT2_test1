b8765312ac148aee1a01fa14d27166817110ca24
LuisSpada <bespa00158@gmail.com>
2022-09-28 11:07:43 +0200

    bruh

--- Benjamin/Monster_baka_spill.py
+++ Benjamin/Monster_baka_spill.py
@@ -2,15 +2,22 @@ class Monster:
     def __init__(self,navn,iq,juks):
         self.navn = navn
         self.iq = iq
-        self.juks = juks'
+        self.juks = juks
     
     def visInfo(self):
-        
+        print(f'Monsteret {self.navn} liker ikke å tape, og med sin iq på {self.iq}, vil han knuse deg!')
+        print(f'{self.navn} kan muligens jukse litt, men {self.navn} er et monster, hva skal du gjøre?')
+
 
 class Spiller:
     def __init__(self,navn,iq,):
         self.navn = navn
         self.iq = iq
+    
+    def visInfo(self):
+        
+
+
 
 arne = Spiller("Arne", 140,0)
 per = Spiller("Per", 110,1)
