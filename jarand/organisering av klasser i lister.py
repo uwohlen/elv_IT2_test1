3e4e74d3c8b83f6179c24c7a5eb965773b234d3e
jarand0905 <jasea001@osloskolen.no>
2022-09-28 13:00:19 +0200

    spill time 1

---
+++ jarand/organisering av klasser i lister.py
@@ -0,0 +1,35 @@
+class Rektangel:
+  """Klasse for å representere et rektangel"""
+  def __init__(self, lengde:int, bredde:int):
+    """Konstruktør"""
+    self.lengde = lengde
+    self.bredde = bredde
+  
+  def areal(self):
+    """Metode for å beregne areal"""
+    return self.lengde * self.bredde
+
+  def visInfo(self):
+    """Skriver ut informasjon om et rektangel"""
+    print(f"Rektangel med lengde {self.lengde} og bredde {self.bredde} har areal {self.areal()}")
+
+class Kvadrat(Rektangel):
+  """Klasse for å representere et kvadrat"""
+  def __init__(self, sidekant:int):
+    super().__init__(sidekant, sidekant)
+  
+  def visInfo(self):
+    """Skriver ut informasjon om et kvadrat"""
+    print(f"Kvadrat med sidelengde {self.lengde} har areal {self.areal()}")
+
+# Liste som skal holde på rektangler
+rektangler = []
+
+# Lager og legger til noen rektangler
+kvadrat = Kvadrat(1)
+rektangler.append(Rektangel(2, 5))
+rektangler.append(Rektangel(4, 3))
+rektangler.append(Rektangel(5, 6))
+
+for r in rektangler:
+  r.visInfo()
