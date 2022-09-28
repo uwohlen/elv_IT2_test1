b2092184c132d86663b4f4dd77485e71a0846af1
Markus <mamaa003@osloskolen.no>
2022-09-28 10:52:31 +0200

    Update gaming.py

--- MarkusM/gaming.py
+++ MarkusM/gaming.py
@@ -3,17 +3,23 @@ from ctypes.wintypes import HPALETTE
 
 
 class monster:
-    def __init__(self,hp,lv,dmg,wk,xp):
+    def __init__(self,hp,lv,dmg,wk,xp,id,name):
+        self.id = id
         self.hp = hp
         self.lv = lv
         self.dmg = dmg
         self.wk = wk
         self.xp = xp
+        self.name = name
+
+    def combat(self):
+        hero.hp = hero.hp - self.dmg
+        print(f"{hero.hp} liv igjen")
 
 
 class hero(monster):
-    def __init__():
-        super().__init__()
+    def __init__(self,hp,lv,dmg,wk,xp,name):
+        super().__init__(hp,lv,dmg,wk,xp,0,name)
     
     def victory(self):
         self.xp += monster.xp
@@ -21,7 +27,7 @@ class hero(monster):
 
 
 class event:
-    def __init__(self,monsterSpawn,monsterAmount,eventText,specialEvent)
+    def __init__(self,monsterSpawn,monsterAmount,eventText,specialEvent,eventID):
         self.monsterSpawn = monsterSpawn
         self.monsterAmount = monsterAmount
         self.eventText = eventText
@@ -33,6 +39,17 @@ class event:
             print(self.eventText)
             print("")
         elif self.specialEvent == True:
-            print("")
+            print("2")
+    
 
 
+monster1 = monster(1,1,1,1,1,1,1)
+monster2 = monster(1,1,1,1,1,1,1)
+hero = hero(1,1,1,1,1,"geir")
+
+monsterList = []
+monsterList.append(monster1)
+
+monsterList[0].combat()
+
+monster1.combat()
