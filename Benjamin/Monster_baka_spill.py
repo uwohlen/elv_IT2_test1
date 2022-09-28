a539c750e5d5c582b1da1df18133b59281dfcb5d
LuisSpada <bespa00158@gmail.com>
2022-09-28 11:02:16 +0200

    gmm

--- Benjamin/Monster_baka_spill.py
+++ Benjamin/Monster_baka_spill.py
@@ -2,7 +2,10 @@ class Monster:
     def __init__(self,navn,iq,juks):
         self.navn = navn
         self.iq = iq
-        self.juks = juks
+        self.juks = juks'
+    
+    def visInfo(self):
+        
 
 class Spiller:
     def __init__(self,navn,iq,):
@@ -12,5 +15,5 @@ class Spiller:
 arne = Spiller("Arne", 140,0)
 per = Spiller("Per", 110,1)
 
-karakter = input(print(f'Velg hvilken karakter du '))
+karakter = input(print(f'Velg hvilken karakter du vil ha: ')
          
