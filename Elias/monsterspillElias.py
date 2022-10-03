1c1fc679ed5fba0b292b1784995e1c55d28c49ef
Elias <eliasholand@hotmail.com>
2022-10-03 23:49:33 +0200

    Endringer

--- Elias/monsterspillElias.py
+++ Elias/monsterspillElias.py
@@ -1,3 +1,7 @@
+from ast import Str
+from multiprocessing.sharedctypes import Value
+
+
 class Monster:
     def __init__(self, navn:str, hp:int = 0, skade_per_slag:int = 0, type:str = "", lokasjon:str = "", liv:bool=True):
         self.navn = navn
@@ -23,12 +27,8 @@ print("i huset kan du finne vaapen, gjenstander, og monster :o")
 print("for å bevege deg skriver du enten inn opp, ned, hoeyre, venstre")
 navn_spiller = "roger" #input("skriv inn navnet ditt: ")
 
-class Rom:
-    def __init__(self, monster:bool=False, kiste:bool=False, gjenstand:bool=False, felle:bool=False):
-        self.monster = monster
-        self.kiste = kiste
-        self.gjenstand = gjenstand
-        self.felle = felle
+
+
 
 
 #setter startverdier for spiller
@@ -38,20 +38,68 @@ spiller = Spiller(navn_spiller, start_hp_spiller, skade_start, "knyttneve")
 
 monster1 = Monster("")
 
-rom1 = Rom().kiste=True
-rom2 = Rom().felle=True
-rom3 = Rom().gjenstand="sverd"
-rom4 = Rom().monster=True
-rom5 = Rom().gjenstand="felle"
-rom6 = Rom()
-rom7 = Rom().gjenstand = "potion of healing"
-rom8 = Rom()
-rom9 = Rom().monster = True
 
-rom_liste =  [
-        [rom1, rom2, rom3],
-        [rom4, rom5, rom6],
-        [rom7, rom8, rom9],
-]
+class Rom:
+    def __init__ (self, romnummer):
+        self.romnummer = romnummer
+
+rom_liste = [[],[],[]]
+
+tall=1
+for i in range(3):
+    rom_liste.append
+    for j in range(3):
+        rom = "rom"
+        rom += str(tall)
+        rom = Rom(tall)
+        rom_liste[i].append(rom)
+        tall+=1
+
+"""
+#romplassering
+    |  [2][0] nr=7 |  [2][1] nr=8 |  [2][2] nr=9 |
+    |  [1][0] nr=4 |  [1][1] nr=5 |  [1][2] nr=6 |
+    |  [0][0] nr=1 |  [0][1] nr=2 |  [0][2] nr=3 |  
+"""
+
+rom_liste[0][0].kiste=True
+rom_liste[0][1].felle=True
+rom_liste[0][2].gjenstand="sverd"
+rom_liste[1][0].monster=True
+rom_liste[1][1].gjenstand="felle"
+rom_liste[2][0].gjenstand = "potion of healing"
+rom_liste[2][2].monster = True
 
 naa_rom = rom_liste[0][0]
+
+
+
+def finnretning():
+    mulige_retninger = "Du kan bevege deg "
+    for i in range(3):
+        for j in range(3):
+            if naa_rom == rom_liste[i][j]:
+                rad_nr = i 
+                kolonne_nr = j
+
+    if rad_nr == 0:
+        mulige_retninger+="opp"
+        if kolonne_nr<2:
+            mulige_retninger+=" eller til høyre"
+    
+    if rad_nr == 1:
+        "blabla"
+    print(mulige_retninger)
+    
+finnretning()  
+
+sant = True
+def kjoor():
+    while sant==True:
+        try:
+            print("x")
+        except:
+            print("dette rommet har ingenting")
+            print("du kan enten bevege deg {retning}")
+            
+
