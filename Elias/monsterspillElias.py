e9b27a2623972137e7f2973a64bca841ec66532d
Elias <eliasholand@hotmail.com>
2022-10-11 21:06:10 +0200

    endring

--- Elias/monsterspillElias.py
+++ Elias/monsterspillElias.py
@@ -1,10 +1,10 @@
 from ast import Str
 from multiprocessing.sharedctypes import Value
 import random 
-
-""" 
 import os
 import sys, time
+""" 
+
 def slow_type(t):
     typing_speed = 30000 #wpm
     for l in t:
@@ -20,19 +20,19 @@ print("Dersom du kommer i et rom med en gjenstand kan du skrive 'grip' for å pl
 print("I rom med monster foelger egen instruks")
 navn_spiller = input("\nFoer du begynner må du skrive inn navnet ditt: ").capitalize()
 
-class Monster:
+class Monster: #lager en klasse for monsteret
     def __init__(self, navn:str, hp:int = 0, skade_per_slag_maks:int = 0, liv:bool=True):
         self.navn = navn
         self.hp=hp
         self.skade_per_slag_maks=skade_per_slag_maks
         self.liv= liv
 
-    def sjekkliv(self): 
+    def sjekkliv(self): #metode som sjekker om monsteret lever eller ikke
         if self.hp<=0:
             self.liv=False
             return True
         
-    def gjooreSkade(self, Spiller):
+    def gjooreSkade(self, Spiller): #metode som gjoer skade pa spilleren
         skade = random.randint(0, self.skade_per_slag_maks)
         Spiller.hp-=skade
         Spiller.sjekkliv()
@@ -40,44 +40,47 @@ class Monster:
         print(f"{navn_spiller} ble truffet av monsteret")
         print(f"{navn_spiller} mistet {skade} hp, så spilleren har nå {Spiller.hp} hp igjen!")
 
-class Spiller:
+class Spiller: #klasse for spiller
     def __init__(self, navn:str, hp:int = 0, gjenstander:list = [], liv:bool=True):
         self.navn = navn
         self.hp = hp
         self.gjenstander = gjenstander
         self.liv = liv
 
-    def sjekkliv(self): 
+    def sjekkliv(self): #sjekker om spilleren lever
         
         if self.hp<=0:
             self.liv=False
             print(f"Du tapte kampen og doede, rip /: Men du kan proeve igjen")
             igjen = input("Ja eller Nei?: ").lower().replace(" ", "")
             if igjen == "ja":
+                self.hp = 100
                 kjoor()
+                
+
             else: 
                 print("too bad /:")
                 exit()
 
-    def spillerPotionofhealing(self):
+    def spillerPotionofhealing(self): #metode for naar spilleren drikker Potion of Healing
         self.hp*=2 
 
-    def printEgenskap(self):
+    def printEgenskap(self): #printer ut hvilke vaapen og skaden det gjoer
         tekst=""
         for i in self.gjenstander:
             tekst+=f"\nGjenstanden {i.navn} gjoer {i.skade} skade"
         print(tekst)
 
-    def felle(self):
+    def felle(self): #metode for naar spilleren traakker i en felle
         spiller.hp /= 2
         print(f'{navn_spiller} har naa {spiller.hp} liv')
 
 
-class Vaapen():
+class Vaapen(): #metode for vaapen
     def __init__(self, navn, skade):
         self.navn=navn
         self.skade=skade
-class Gjenstand():
+class Gjenstand(): #metode for gjenstand. Brukes for Potion of Healing
     def __init__(self,navn):
         self.navn=navn
 
@@ -89,12 +92,14 @@ spiller = Spiller(navn_spiller, start_hp_spiller,[Vaapen("knyttneve", 20)])
 monster1 = Monster("Peder", 50, 40)
 monster2 = Monster("morbius", 169, 60)
 
-class Rom:
+class Rom: #klasse for rommet
     def __init__ (self, romnummer):
         self.romnummer = romnummer
-        
+
+#lager en tom liste for oppsettet til romfordelingen
 rom_liste = [[],[],[]]
 
+#fordeler rommene gjennom en while loekke
 tall=1
 for i in range(3):
     rom_liste.append
@@ -111,7 +116,7 @@ for i in range(3):
     |  [1][0] nr=4 |  [1][1] nr=5 |  [1][2] nr=6 |
     |  [0][0] nr=1 |  [0][1] nr=2 |  [0][2] nr=3 |  
 """
-
+#legger til egenskaper ved de forskjellige rommene
 rom_liste[0][0].kiste=True
 rom_liste[0][1].felle=True
 rom_liste[0][2].gjenstand=Vaapen("sverd",40)
@@ -127,6 +132,7 @@ valg_muligheter=[]
 rad_nr=0
 kolonne_nr=0
 
+#funksjon for aa printe ut mulige retninger spilleren kan gaa. Tar hensyn til "border" til huset. 
 def finnretning():
     mulige_retninger = f"{navn_spiller} kan bevege deg "
     global rad_nr, kolonne_nr
@@ -154,7 +160,7 @@ def finnretning():
     print(mulige_retninger)
     
 
-
+#leter etter objekter i rommet
 def finnVariabel():
     liste=dir(naa_rom)
     for i in liste:
@@ -162,11 +168,13 @@ def finnVariabel():
             verdi = i
             return verdi
 
+#funksjon for aa plukke opp en gjenstand
 def plukkOppGjenstand():
     if finnVariabel()=="gjenstand":
         spiller.gjenstander.append(naa_rom.gjenstand)
         print(f"{naa_rom.gjenstand.navn} er plukket opp")
 
+#funksjon for aa aapne en kiste 
 def aapneKiste():
     liste = [Gjenstand("potion of healing"),Monster("Peder", 50, 40)]
     gjenstand = liste[random.randint(0, 1)]
@@ -180,17 +188,18 @@ def aapneKiste():
         naa_rom.monster=gjenstand
         selveKampen()
 
+#funksjon for aa lage en liste med alle vaapnene
 def printVaapen():
     liste=[]            
     for i in spiller.gjenstander:
         liste.append(i.navn)
     return liste
 
+#funksjon for selve kampen med monsteret
 def selveKampen():
     print("Kampen har begynt!")
     print(f"\nDu tar det foorste slaget!")
     while naa_rom.monster.liv == True:
-        printVaapen()
         mulige_vaapen = printVaapen()
 
         if printVaapen()[-1] == "knyttneve": 
@@ -212,6 +221,9 @@ def selveKampen():
                     naa_rom.monster.gjooreSkade(spiller) 
         else: print("Skriv inn et gyldig alternativ")
     del(naa_rom.monster)
+
+#funksjon for aa informere om at spilleren er i rom med et monster
+#funksjonen stiller spoersmaal om spilleren vil sloss eller ikke
 def monsterKamp():
     print(f'Du har kommet i et rom med et monster med navnet {naa_rom.monster.navn}')
     print(f'Du kan velge å loope eller å sloss!')
@@ -223,13 +235,12 @@ def monsterKamp():
     else: 
         print("\nNeivel, kanskje en annen gang")
 
-
+#funksjonen til selve spillet
 sant = True
 def kjoor():
     global naa_rom, rad_nr, kolonne_nr, sant
     while sant==True:
         print(f"\n{navn_spiller} er rom nr {naa_rom.romnummer}")
-        
         if finnVariabel()!=None:
             if finnVariabel()=="gjenstand":
                 if naa_rom.gjenstand.navn=="potion of healing":
@@ -299,3 +310,8 @@ def kjoor():
 
 
 kjoor()
+
+
+
+
+#maa fikse aa restarte gamet
