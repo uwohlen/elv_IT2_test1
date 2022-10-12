a4cddba77b5b7fe9a73d83e3fe597b80ac9fb3b9
LuisSpada <bespa00158@gmail.com>
2022-10-12 18:45:33 +0200

    fddfdf

--- Benjamin/Monsterspill.py
+++ Benjamin/Monsterspill.py
@@ -3,7 +3,7 @@ import os
 import sys, time
 
 def slow_type(t):
-    typing_speed = 150 #wpm
+    typing_speed = 1500 #wpm
     for l in t:
         sys.stdout.write(l)
         sys.stdout.flush()
@@ -14,9 +14,6 @@ class Monster: #konstruktør for monstere
         self.navn = navn
         self.iq = iq
         self.juks = juks
-    def __str__(self):
-        return(f'Din motstander er {self.navn} som ikke liker å tape, og med sin iq på {self.iq}, \nvil han knuse deg! Han kan muligens jukse litt, men {self.navn} er et monster,\nhva skal du gjøre?                                                                                                          .')
-
 
 class Spiller: #konstruktør for karakterer
     def __init__(self,navn,iq,flaks):
@@ -27,28 +24,63 @@ class Spiller: #konstruktør for karakterer
     def __str__(self):
         return(slow_type(f'Karakteren {self.navn} har en iq på hele {self.iq}.'))
 
-arne = Spiller("Arne",140,3)
-per = Spiller("Per",90,1)
+arne = Spiller("Arne",140,1)
+per = Spiller("Per",110,3)
 greven = Monster("Greven",100,1)
 
-slow_type('Dette spillet går ut på å spille blackjack mot et monster, er du klar?\n')
 
-slow_type(f'Karakteren Arne har en iq på hele 140!\n')
-slow_type(f'Karakteren Per har en iq på bare 70, men Per har til gjengjeld super mye flaks!\n')
+'''slow_type('Velkommen til monster BlackJack!\n')'''
+slow_type(f'En dag blir du sugd inn i en annen verden, og den eneste måten å komme seg hjem igjen er å spille BlackJack\n')
+slow_type(f'Desverre for deg er du nødt til å bekjempe en rekke monstere for å seire og returnere hjem trygt\n')
+print('\n')
+
+print(f'[1]Karakter: {arne.navn}    IQ: {arne.iq}      Flaks: Middels')
+print(f'[2]Karakter: {per.navn}     IQ: {per.iq}      Flaks: Høy')
+print(f'[I]Info om egenskaper\n')
 
 karakter = 0
 while True:
-    karakter = input('Velg hvilken karakter du vil bruke:\nFor Arne trykk 1, og for Per trykk 2:')
+    karakter = input('Velg hvilken karakter du vil bruke i ditt eventyr: ').lower()
     if karakter == "1":
-        slow_type(' Du valgte Arne, han er min favoritt :)\n')
-        slow_type(f'{greven}\n')
+        slow_type('Du valgte Arne, han er min favoritt :)                       \n')
         break
     elif karakter == "2":
-        slow_type('Du valgte Per, han er en flink gutt :)\n')
-        slow_type(f'{greven}\n')
+        slow_type('Du valgte Per, han er en flink gutt :)                       \n')
         break
+    elif karakter == "i":
+        slow_type(f'IQ: Øker sjanse for å tape mindre penger\n')
+        slow_type(f'Flaks: Øker sjanse for å vinne mer penger\n')
     else:
-        slow_type("bruh, du hadde 1 oppgave :/\n")
+        slow_type("Bruh, du hadde 1 oppgave :/\n")
+
+slow_type(f'Ditt første hinder er selveste Greven som er kjent i BlackJack verdenen for å knuse nybegynnere som deg!\n')
+slow_type(f'Det sies at Monster-BlackJack er litt annerledes enn vanlig BlackJack, fordi man vinner kun når man har tatt livet til motstanderen sin\n')
+
+
+def regler():
+    slow_type(f'Generelle Blackjack regler\n')
+    slow_type(f'\n')
+    slow_type(f'Blackjack er et klassisk casinobordspill, og spilles av mange casinospillere verden over. \n') 
+    slow_type(f'Kortspillets regler er enkle å lære seg: Målet er å få en kortverdi lik 21, eller nærmest. \n') 
+    slow_type(f'Enkelt forklart fungerer spillet slik:\n')
+ 
+    slow_type(f'● Før spillet starter må du legge en innsats på bordet. \n')
+    slow_type(f'● Deretter blir det delt ut to kort hver til dealer og spiller.\n')
+    slow_type(f'● Dealeren får ett av sine kort med billedsiden opp. \n')
+    slow_type(f'● Etter de to første kortene er delt ut kan du velge om du ønsker å få tildelt et tredje kort. \n')
+    slow_type(f'● Utfallet i kortspillet er basert på tilfeldigheter, noe som gjør spillet ekstra spennende. \n')
+    slow_type(f'● Du spiller alene mot dealeren. \n')
+    slow_type(f'● Du kan ikke overstige 21. \n')
+    slow_type(f'● Blackjack kan spilles med to eller flere kortstokker (i noen spillvarianter bruker man opptil åtte kortstokker).\n')
+    slow_type(f'● I mange Blackjack-varianter har du muligheten til å splitte hånden for å unngå å overstige verdien av 21. \n')
+ 
+    slow_type(f'Dealeren avslører sine kort til sist, og det er da den som er nærmest 21 som vinner. \n')
+ 
+    slow_type(f'Kortverdiene fungerer slik: \n')
+ 
+    slow_type(f'● Alle tallkort er verdt sin egen verdi \n')
+    slow_type(f'● Alle billedkort er verdt 10\n')
+    slow_type(f'● Ess er verdt 1 eller 11, avhengig av spillet\n')
 
 
 
@@ -56,12 +88,8 @@ deck = [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14]*4
 vinn = 0
 tap = 0
 penger = 1000
-items = []
-butikk = ["[G]glock9: 50.000$","[A]ammo: 5.000$","[V]vitaminer: 2.500$",1]
-bonus = 0
-g = butikk.index("[G]glock9: 50.000$")
-a = butikk.index("[A]ammo: 5.000$")
-v = butikk.index("[V]vitaminer: 2.500$")
+
+
 
 def deal(deck): #Definerer funksjonen for å dele ut kort
     hand = []
@@ -79,6 +107,8 @@ def deal(deck): #Definerer funksjonen for å dele ut kort
         hand.append(kort)
     return hand
 
+
+
 def play_again(): #Definerer funksjonen for å spille på nytt
     while True:
         again = input("Vil du spille igjen eller besøke butikken? [J]ja, [N]Nei og [B]Butikk : ").lower()
@@ -95,6 +125,15 @@ def play_again(): #Definerer funksjonen for å spille på nytt
         elif again == "b":
             sjappe()
 
+
+
+items = []
+butikk = ["[G]glock9: 50.000$","[A]ammo: 5.000$","[V]vitaminer: 2.500$"]
+bonus = 0
+g = butikk.index("[G]glock9: 50.000$")
+a = butikk.index("[A]ammo: 5.000$")
+v = butikk.index("[V]vitaminer: 2.500$")
+
 def sjappe():
     global penger
     global bonus
@@ -107,7 +146,7 @@ def sjappe():
         gjenstand = input(f'For å kjøpe gjenstander skriv bokstaven i parantesen til gjenstanden. \nFor å gå tilbake trykk[T]: ').lower()
         if gjenstand == "g" and penger >= 50000:
             items.append("Glock9")
-            butikk.pop(g)
+            butikk.remove("[G]glock9: 50.000$")
             penger -= 50000
             slow_type("Takk for kjøpet!\n")                        
             slow_type(f'Din nye balanse er: {penger}$                                                      \n')
@@ -117,7 +156,7 @@ def sjappe():
             break
         if gjenstand == "a" and penger >= 5000:
             items.append("Ammo")
-            butikk.pop(a)
+            butikk.remove("[A]ammo: 5.000$")
             penger -= 5000
             slow_type("Takk for kjøpet!\n")
             slow_type(f'Din nye balanse er: {penger}$                                                         \n')
@@ -127,7 +166,7 @@ def sjappe():
             break
         if gjenstand == "v" and penger >= 2500:
             items.append("Vitaminer")
-            butikk.pop(v)
+            butikk.remove("[V]vitaminer: 2.500$")
             penger -= 2500
             bonus += 2
             slow_type("Takk for kjøpet!\n")
@@ -220,7 +259,7 @@ def flaks():
     global penger
     global gamble
     if karakter == "1":
-        n = random.randint(3,int(arne.flaks))
+        n = random.randint(1,int(arne.flaks))
         if n > 2:
             penger += (1.1 + (int(arne.flaks)/10)) * gamble
         else:
@@ -232,19 +271,22 @@ def flaks():
         else:
             gevinst()
 
+
 def iq():
+    global penger
+    global gamble
     global penger
     global gamble
     if karakter == "1":
         m = random.randint(1,int(arne.iq))
         if m > int(arne.iq)*0.7:
-            penger -= (1 - (2 * int(arne.flaks) / 1000)) * gamble
+            penger -= (1 - (2 * int(arne.iq) / 1000)) * gamble
         else:
             tapepenger()
     if karakter == "2":
         m = random.randint(1,int(per.iq))
         if m > int(per.iq)*0.7:
-            penger -= (1 - (2 * int(per.flaks) / 1000)) * gamble
+            penger -= (1 - (2 * int(per.iq) / 1000)) * gamble
         else:
             tapepenger()
 
@@ -314,6 +356,16 @@ def score(dealer_hand, player_hand): #Definerer funksjonen for å printe ut ende
         slow_type(f'Din nye balanse er: {penger}$\n')
     
 
+while True:
+    starte_spill = input(f'Er du opp for utfordringen, eller trenger du kanskje regler til BlackJack først? [J]Ja / [N]Nei / [R]Regler: ').lower()
+    if starte_spill == "j":
+        break
+    elif starte_spill == "n":
+        slow_type(f'Vel jeg ville ikke gått unna denne sjansen om jeg var deg, men da får du vel bare kose deg i monsterverdenen haha')
+        exit()
+    elif starte_spill == "r":
+        regler()
+
 def game(): #Definerer spillets gang :)
     global vinn
     global tap
@@ -327,6 +379,8 @@ def game(): #Definerer spillets gang :)
     slow_type(f' seiere: {vinn}       tap: {tap}\n')
     slow_type(f'Balanse: {penger}$\n')
     print("Dine gjenstander:")
+    print(arne.flaks)
+    print(per.flaks)
     for i in range(len(items)):
         print(items[i])
     print("\n")
