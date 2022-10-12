0383f8002d7129a4e8c7f314d41431a63194d353
LuisSpada <bespa00158@gmail.com>
2022-10-12 18:46:32 +0200

    sdsdsd

--- Benjamin/Monsterspill.py
+++ Benjamin/Monsterspill.py
@@ -451,4 +451,4 @@ if __name__ == "__main__":
 #Iq skal gjøre at man får ekstra bonuser/multipliers for gevinstene sine
 #Juksing kan gjøre at greven ikke gir tilbake den summen man skal ha
 #legge til voicelines for Greven basert på randint genererte nummere, disse blir printa etter hvert game
-#legge til en butikk hvor du kan kjøpe oppgraderinger, og glock for å drepe Greven xxxxxxxxxxxxxxx
+#legge til en butikk hvor du kan kjøpe oppgraderinger, og glock for å drepe Greven xxxxxxxxxxxxxxxds
