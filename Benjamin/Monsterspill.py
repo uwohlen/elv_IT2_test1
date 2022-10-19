c7ed4e1a58f89ceb4936848aaf9f5dbb304c26e0
LuisSpada <bespa00158@gmail.com>
2022-10-19 10:25:25 +0200

    ssdsdsj

--- Benjamin/Monsterspill.py
+++ Benjamin/Monsterspill.py
@@ -1,5 +1,3 @@
-#xianzai wo you bingqilin
-
 import random
 import os
 import sys, time
@@ -720,5 +718,4 @@ if __name__ == "__main__":
 
 #Juksing kan gjøre at greven ikke gir tilbake den summen man skal ha
 #legge til voicelines for Greven basert på randint genererte nummere, disse blir printa etter hvert game
-#Man kan bruke glock og ammo for å gjøre skade, ikke nødvendigvis direkte drepe, men greven dør når han er tom for liv
 #legge til et interface med info om grevenog at du interacte med han mer
