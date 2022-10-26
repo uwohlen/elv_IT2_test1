d5c8824f81f9b0eda509dda859a3f3e5399e0925
MarkusSMadsbakken <mamaa003@osloskolen.no>
2022-10-26 18:05:14 +0200

    gaming

--- MarkusM/gaming2.py
+++ MarkusM/gaming2.py
@@ -1,4 +1,3 @@
-from operator import indexOf
 import pygame as pg
 import random as r
 import sys, time
@@ -11,7 +10,7 @@ window_width = 1080
 window_height = 720
 window = pg.display.set_mode([window_width,window_height],pg.RESIZABLE)
 pg.display.set_caption('gaming')
-font = pg.font.SysFont("Arial", 24)
+font = pg.font.SysFont("MarkusM/font_test/coolvetica rg.otf", 40)
 
 gifList = []
 keyIndex = 0
@@ -65,7 +64,10 @@ def fps():
 
 
 """
+windowFPS = 30 #hvor mange frames pr sekund som blir rendera
+counter = 0
 typetest = str()
+task = "hei"
 
 while True: #displayLoop
 
@@ -76,9 +78,16 @@ while True: #displayLoop
         
         if event.type == pg.KEYDOWN:
             try:
-                i = keyInit.index(pg.key.name(event.key))
-                typetest += keyList[i].key
-                print(typetest)
+                keys = pg.key.get_pressed()
+                if keys[K_LSHIFT]:
+                    i = keyInit.index(pg.key.name(event.key))
+                    typetest += keyInit[i].upper()
+
+                    
+                else:
+                    i = keyInit.index(pg.key.name(event.key))
+                    typetest += keyInit[i]
+                #print(typetest) #teste om stringen oppdaterer seg som den skal
 
             except:
                 pass
@@ -87,6 +96,8 @@ while True: #displayLoop
 
             if event.key == pg.K_BACKSPACE:
                 typetest = typetest[0:-1]
+
+                #hvis man holder i fler frames, sletter den fortere
             if event.key == pg.K_SPACE:
                 typetest = typetest + str(" ")
             
@@ -97,7 +108,6 @@ while True: #displayLoop
             #keyList[event.key].keyUp
                 #finne hvilken som er trykket
 
-            
     #frames_counter +=1
     #if frames_counter == 61 or frames_counter >= 61:
     #    frames_counter =0
@@ -128,13 +138,23 @@ while True: #displayLoop
 
     #for i in range(len(typetest)):
     #    window.blit(font.render(str(typetest[i]),True,(0,0,0)),(10*i,200))
-    window.blit(font.render(str(typetest),True,(0,0,0)),(20,200)) 
 
+    window.blit(font.render(str(task),True,(0,0,0)),(window_width/2,window_height/2-200))
+    typeText = font.render(str(typetest),True,(0,0,0))
+    typeRect = typeText.get_rect(center=(window_width/2,window_height/2))
+    window.blit(typeText,typeRect)
 
+    #lage en metode for å ender farge hvis det man skriver er riktig
 
-    clock.tick(24)
-    pg.display.flip()
+    clock.tick(windowFPS) #Oppdaterer skjermen og teller hvilken frame vi er på
+    pg.display.flip() 
+
+    counter +=1
+    #print(counter)
+    if counter == windowFPS:
+        counter = 0
 
     
 
 #https://www.cleverpdf.com/gif-to-png
+#https://www.dafont.com/top.php
--- MarkusM/tesdt.py
+++ MarkusM/tesdt.py
@@ -1,1 +1,3 @@
 print("😄")
+import pygame
+pygame.font.Font("MarkusM/font_test/coolvetica rg.otf",12)
