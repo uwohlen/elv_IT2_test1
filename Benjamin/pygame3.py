975534aff763313cbd0c2ac7d34321a8efd0483a
LuisSpada <bespa00158@gmail.com>
2022-10-27 00:10:54 +0200

    sdsdwwd

--- Benjamin/pygame3.py
+++ Benjamin/pygame3.py
@@ -8,6 +8,7 @@ from pygame.locals import *
 # Initialiserer/starter pygame
 pg.init()
 gifList = []
+gifList2 = []
 counter = 0
 clock = pg.time.Clock()
 # Oppretter et vindu der vi skal "tegne" innholdet vårt
@@ -18,6 +19,8 @@ vindu = pg.display.set_mode([VINDU_BREDDE, VINDU_HOYDE])
 print(type(vindu))
 for i in range(2):
     gifList.append(pg.image.load(f"Benjamin/pngs/måneskin/måneskin-{i}.png"))
+for i in range(-1,1):
+    gifList2.append(pg.transform.scale(gifList[i], (1280, 720)))
 
 # Angir hvilken skrifttype og tekststørrelse vi vil bruke på tekst
 font = pg.font.SysFont("Arial", 24)
@@ -31,7 +34,7 @@ while fortsett:
             fortsett = False
 
     clock.tick(2)
-    vindu.blit(gifList[counter],(0, 0))
+    vindu.blit(gifList2[counter],(0, 0))
     counter += 1
     if counter == 2:
         counter = 0
