4ce291374f6ff439b17813a5551d96feee865526
LuisSpada <bespa00158@gmail.com>
2022-10-26 23:31:08 +0200

    asasas

--- Benjamin/pygame3.py
+++ Benjamin/pygame3.py
@@ -8,24 +8,23 @@ from pygame.locals import *
 # Initialiserer/starter pygame
 pg.init()
 gifList = []
-
+counter = 0
+clock = pg.time.Clock()
 # Oppretter et vindu der vi skal "tegne" innholdet vårt
 VINDU_BREDDE = 1280
 VINDU_HOYDE  = 720
 vindu = pg.display.set_mode([VINDU_BREDDE, VINDU_HOYDE])
 
 print(type(vindu))
+for i in range(2):
+    gifList.append(pg.image.load(f"Benjamin/pngs/måneskin/måneskin-{i}.png"))
 
-
-for i in range(0,1):
-    måne = gifList.append(pg.image.load(f"Benjamin/pngs/måneskin/måneskin-{i}.png"))
 # Angir hvilken skrifttype og tekststørrelse vi vil bruke på tekst
 font = pg.font.SysFont("Arial", 24)
 
 # Gjenta helt til brukeren lukker vinduet
 fortsett = True
 while fortsett:
-
     # Sjekker om brukeren har lukket vinduet
     for event in pg.event.get():
         if event.type == pg.QUIT:
@@ -43,10 +42,12 @@ while fortsett:
     '''mittBilde = pg.image.load("Benjamin/pngs/måneskin.gif")
     vindu.blit(mittBilde, (0, 0))
         vindu.blit(måne, (0, 0))'''
-
-    # Oppdaterer alt innholdet i vinduet
-    pg.display.flip()
-
+    clock.tick(2)
+    vindu.blit(gifList[counter],(0, 0))
+    counter += 1
+    if counter == 2:
+        counter = 0
+    print(counter)
     # Oppdaterer alt innholdet i vinduet
     pg.display.flip()
 
