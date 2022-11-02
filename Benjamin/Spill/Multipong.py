859e32fb8069dba6a6fd5e08cc1c0dda1124d7a7
LuisSpada <bespa00158@gmail.com>
2022-11-02 12:59:10 +0100

    qsq

--- Benjamin/Spill/Multipong.py
+++ Benjamin/Spill/Multipong.py
@@ -1,4 +1,6 @@
 import pygame as pg
+from pygame.locals import (K_UP, K_DOWN, K_LEFT, K_RIGHT)
+import math as ma
 
 # Initialiserer/starter pygame
 pg.init()
@@ -26,19 +28,21 @@ arena = Arena(360,0,560,720,(120,120,120))
     
 class Pong:
   """Klasse for å representere en ball"""
-  def __init__(self, x, y, fartx, farty, bredde, vindusobjekt, farge):
+  def __init__(self, x, y, fartx, farty, bredde, høyde, vindusobjekt, farge):
     """Konstruktør"""
     self.x = x
     self.y = y
     self.fartx = fartx
     self.farty = farty
     self.bredde = bredde
+    self.høyde = høyde
     self.vindusobjekt = vindusobjekt
     self.farge = farge
   
   def tegn(self):
     """Metode for å tegne kvadratene"""
     pg.draw.rect(pong1.vindusobjekt, pong1.farge, (pong1.x, pong1.y, pong1.bredde, pong1.bredde))
+    pg.draw.rect(plate.vindusobjekt, plate.farge, (plate.x, plate.y, plate.bredde, plate.høyde))
 
   def flytt(self):
     """Metode for å flytte kvadratene"""
@@ -50,24 +54,11 @@ class Pong:
     # Flytter pong1en
     pong1.x += pong1.fartx
     pong1.y += pong1.farty
-    if ((pong2.x - pong2.bredde) <= arena.x) or ((pong2.x + pong2.bredde) >= arena.x + arena.bredde):
-      pong2.fartx = -pong2.fartx
-    elif ((pong2.y - pong2.bredde) <= arena.y) or ((pong2.y + pong2.bredde) >= arena.y + arena.høyde):
-      pong2.farty = -pong2.farty
-    # Flytter ballen
-    pong2.x += pong2.fartx
-    pong2.y += pong2.farty
-    if ((pong3.x - pong3.bredde) <= 0) or ((pong3.x + pong3.bredde) >= pong3.vindusobjekt.get_width()):
-      pong3.fartx = -pong3.fartx
-    elif ((pong3.y - pong3.bredde) <= 0) or ((pong3.y + pong3.bredde) >= pong3.vindusobjekt.get_height()):
-      pong3.farty = -pong3.farty
-    # Flytter ballen
-    pong3.x += pong3.fartx
-    pong3.y += pong3.farty
 
-pong1 = Pong(540, 540, 0.1, 0.1, 45, vindu, (255,255,255))
-pong2 = Pong(360, 360, 0.5, 0, 20, vindu, (0,0,0))
-pong3 = Pong(360, 360, 0, 0.5, 20, vindu, (0,0,0))
+pong1 = Pong(540, 540, 0.1, 0.1, 45, 45, vindu, (255,255,255))
+pong2 = Pong(360, 360, 0.5, 0, 20,0, vindu, (0,0,0))
+pong3 = Pong(360, 360, 0, 0.5, 20,0, vindu, (0,0,0))
+plate = Pong(595,650,0.15,0,90,10,vindu,(255,255,255))
 
 # Angir hvilken skrifttype og tekststørrelse vi vil bruke på tekst
 font = pg.font.SysFont("Arial", 24) 
@@ -81,11 +72,18 @@ while fortsett:
         if event.type == pg.QUIT:
             fortsett = False
 
+    # Henter en ordbok med status for alle tastatur-taster
+    trykkede_taster = pg.key.get_pressed()
+
     # Farger bakgrunnen hvit
     vindu.fill((120, 120, 120))
 
     # Tegner et rektangel
     pg.draw.rect(vindu, (0, 0, 0), (360, 0, 560, 1280))
+    if trykkede_taster[K_LEFT]:
+      plate.x -= plate.fartx
+    if trykkede_taster[K_RIGHT]:
+      plate.x += plate.fartx
     pong1.tegn()
     pong1.flytt()
 
