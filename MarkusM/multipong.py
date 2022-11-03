39a7cc93e0712f8dd1cd644c95aa71ba6babef91
Markus <mamaa003@osloskolen.no>
2022-11-03 11:25:37 +0100

    gaming2

---
+++ MarkusM/multipong.py
@@ -0,0 +1,26 @@
+from turtle import window_height, window_width
+import pygame as pg
+import math
+import random as r
+import sys
+
+window_width = 400
+window_height = 600
+
+pg.init()
+
+window = pg.display.set_mode([window_width,window_height])
+run = True
+
+
+
+
+
+while run:
+    for event in pg.event.get():
+            if event.type == pg.QUIT:
+                sys.exit()
+
+
+
+
